90c96b915a4600ed6b3211bd67e1d652a4f1d1f6
Jonathan Guaje <jonathanguajeduque@gmail.com>
2023-07-12 20:30:40 -0400

    se agrego el js que era

--- js/codigo.js
+++ js/codigo.js
@@ -1,61 +1,28 @@
-const nombreProgramador = "Daniel Llobet";
-const nombreTutor = "Andres Pata"
-console.log(nombreProgramador);
-let usuario = prompt("¿Cual es tu nombre?");
-let edad;
-console.log(usuario);
-if(usuario == nombreProgramador || usuario == nombreTutor)
-    alert ("Bienvenido Programador Master");
-else{
-    edad = parseInt(prompt("¿Cual es tu edad?"));
-    console.log(edad);
-    let nacimiento = ( 2023 - edad);
-    console.log(nacimiento);
-    if(edad >= 18){
-        alert("Naciste en " + nacimiento + ", eres mayor de edad" + ". PUEDES INGRESAR =)")
-    }       
-    else{
-        alert("Naciste en " + nacimiento + ", eres menor de edad" + ". NO PUEDES INGRESAR >=(")
-    }
+let llenarDeNuevo = true;
+let cantidadPedidos, cantidadSku, nombrePicker, resultadoPedidos, resultadoSku, resultadoTotal;
+function pedirNombre(){
+    nombrePicker = prompt('Bienvenido Picker, por favor ingresa tu nombre');
 }
-let edad2 = edad;
-for(edad; edad >= 1; edad--){
-    console.log("Te saludo " + edad2 + " veces, por tus " + edad2 + " años");
-}
-if(usuario == nombreProgramador || usuario == nombreTutor)
-    alert("Que Todo Te Salga......");
-    else{
-        alert("BIENVENIDO A FLETES Y MUDANZAS")
-        let ciudad = prompt("Trabajamos desde SANTIAGO CENTRO hasta las siguientes ciudades: TALCA (1), CONCEPCION (2) Y MENDOZA (3). ¿Por favor digite el numero de su opcion?")
-        while(ciudad != "0" ){
-            switch(ciudad){
-                case "1":
-                    alert("El precio de la mudanza hasta Talca seria: 1000$ USD");
-                    alert("Gracias por cortizar");
-                    break;
-                case "2":
-                    alert("El precio de la mudanza hasta Concepcion seria: 1500$ USD");
-                    alert("Gracias por cortizar");
-                    break;
-                case "MENDOZA":
-                    alert("El precio de la mudanza hasta Mendoza seria: 2500$ USD");
-                    alert("Gracias por cortizar");
-                    break;
-                default:
-                    alert("¡Opcion Mal Ingresada (Opciones 1, 2 o 3)!");
-            }
-            ciudad = prompt("Ingrese la Opcion (1, 2 o 3) para cotizar o  escriba 0 para salir");
-        }
-}
-if(usuario == nombreProgramador || usuario == nombreTutor)
-    alert("Muy Bien......");
-    else{
-        experiencia()
-        function experiencia(){
-        let exp = prompt("califica del 1 al 10 tu experiencia, donde 10 es la mayor calificacion");
-        console.log("La calificacion del usuario fue:", exp);
-        
-    }
-}
+function iff(){
 
-
+if (resultadoTotal < 20000){
+    calificacion = "Fue un mal día =(";
+    }
+else if (resultadoTotal >= 20000 && resultadoTotal <=40000){
+    calificacion = "Fue un día aceptable =/ ";
+    }
+else (calificacion = "¡¡¡Fue un excelente día!!! =D")}
+    }
+    alert("La monetización fue de " + resultadoTotal +" $. " + calificacion);
+}
+pedirNombre();
+while (llenarDeNuevo) {
+cantidadPedidos = parseFloat(prompt( `${nombrePicker}, ¿Cuántos pedidos hiciste en el día?`));
+cantidadSku = parseFloat(prompt(`${nombrePicker}, ¿Cuántos SKU hiciste en el día?`));
+resultadoPedidos = (cantidadPedidos*100);
+resultadoSku = (cantidadSku*75);
+resultadoTotal= resultadoPedidos + resultadoSku;
+iff();
+llenarDeNuevo = confirm('¿Deseas volver a llenar los datos?');
+}
+alert('¡Gracias por usar esta Script!');
